d323beb42a54bed8827d1e2a4c39c2b1aa48f81e
Yash Jethwa <150799898+Yash-Jethwa@users.noreply.github.com>
2024-07-24 16:29:10 +0530

    Add files via upload

---
+++ frontend/src/page/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About
+      <br></br>YASH JETHWA
+      <br></br>HARICHANDRA ZALA
+      <br></br>SANJAY GADHAVI
+
+    </div>
+    
+  )
+}
+
+export default About
---
+++ frontend/src/page/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>Contact HERE !! TO ADMIN 
+      <br></br>yashjethwa1357@gmail.com
+    </div>
+    
+
+  )
+}
+
+export default Contact
